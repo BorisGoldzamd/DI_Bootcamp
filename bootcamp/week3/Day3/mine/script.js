768c32b1f71ff21c2fd2137bb1827c25083a2b66
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2023-12-13 08:03:11 +0200

    day4

---
+++ bootcamp/week3/Day3/mine/script.js
@@ -0,0 +1,56 @@
+// conditionals 
+//if else... if the conditionals is true, we excecute a peace of code, if is not we execute else. 
+// let age = prompt('How old are you?');
+
+// if (age >= 18 && age <= 25) {
+//     console.log('You are a big human');
+// } else if (age <= 10) {
+//     console.log('Go back to primary school');
+// } else if (age >= 65) {
+//     console.log('You are too big, buddy');
+// } else if (isNaN(age)) {
+//     console.log('Don\'t troll me, that\'s not a number');
+// } else {
+//     console.log('Ok, I don\'t have an answer for this');
+// }
+
+// let age = prompt('how old are you?')
+// if (isNaN(age)){
+//     alert('dont troll me')
+// }
+// else if (age < 18){
+//     alert("Sorry, you are too young to drive this car")
+// } else if (age === 18){
+//     alert('Congratulations on your first year of driving')
+// }else{
+//     alert('Power on, enjoy your ride')
+// }
+// const fruit = 'Papayas'
+// switch (fruit){
+//     case 'Oranges':
+//         console.log('Oranges are $0.57');
+//         break
+//     case 'Mango':
+//     case 'Papayas':
+//         console.log('they are cheap, mangos an papayas buyyy');
+//         break;
+//     default:
+//         console.log('Sorry we dont have more fruits');
+//         break;
+// };
+
+let user1 = {
+    username: 'Boris',
+    password: '12345'
+}
+let user2 = {
+    username: 'Tamara',
+    password: '11111'
+}
+let user3 = {
+    username: 'Jorge',
+    password: '00000'
+}
+
+let database = [user1];
+let newsfeed = [user1, user2, user3]
---
+++ bootcamp/week3/Day4/script.js
@@ -0,0 +1,7 @@
+let x = 5
+let y = 2
+
+if (x > y ){
+    console.log('x is a bigger number')
+}else (console.log('y is bigger'))
+
