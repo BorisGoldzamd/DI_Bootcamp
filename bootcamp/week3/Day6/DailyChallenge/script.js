95dfacc039033c93325ee0b8f8d66c852300b130
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2023-12-16 17:45:54 +0200

    dailyChallenge

---
+++ bootcamp/week3/Day6/DailyChallenge/script.js
@@ -0,0 +1,12 @@
+for(let i=0; i < 7; i ++){
+    const asteriscos = '*'.repeat(i);
+    console.log(asteriscos)
+}
+
+for (let i = 1; i <= 8; i++) {
+    let asteriscos = '';
+    for (let j = 0; j < i; j++) {
+        asteriscos += '*';
+    }
+    console.log(asteriscos);
+}
