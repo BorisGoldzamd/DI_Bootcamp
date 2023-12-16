1c247c2793a45bbce2685ccefbb29b46aa7f833e
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2023-12-16 17:34:10 +0200

    ExcXP finish

--- bootcamp/week3/Day6/ExcersiseXP/script.js
+++ bootcamp/week3/Day6/ExcersiseXP/script.js
@@ -58,3 +58,70 @@
 // }
 
 // alert('Ok, end.');
+// const building = {
+//     numberOfFloors: 4,
+//     numberOfAptByFloor: {
+//         firstFloor: 3,
+//         secondFloor: 4,
+//         thirdFloor: 9,
+//         fourthFloor: 2,
+//     },
+//     nameOfTenants: ["Sarah", "Dan", "David"],
+//     numberOfRoomsAndRent:  {
+//         sarah: [3, 990],
+//         dan:  [4, 1000],
+//         david: [1, 500],
+//     },
+// }
+// console.log('the number of floors is ' + building.numberOfFloors);
+// console.log('aparments in floor 1 ' + building.numberOfAptByFloor.firstFloor + 'numbers of aparments in floor 3 ' + building.numberOfAptByFloor.thirdFloor);
+// console.log(building.nameOfTenants[1] + ' '+building.numberOfRoomsAndRent.dan[0])
+// if(building.numberOfRoomsAndRent.sarah[1] < building.numberOfRoomsAndRent.dan[1] && building.numberOfRoomsAndRent.david[1] < building.numberOfRoomsAndRent.dan[1]){
+//     building.numberOfRoomsAndRent.dan[1] = 1200;
+//     console.log('we incresed dan rent to ' + building.numberOfRoomsAndRent.dan[1] )
+// }else{
+//     console.log('dan pays more than sarah and david')
+// }
+
+// //excersise5
+// const family = {
+//     familyName: "Smith",
+//     numberOfMembers: 4,
+//     city: "New York",
+//     hasPets: true,
+//     favoriteHobbies: ["Reading", "Cooking", "Traveling"],
+//     mainIncomeSource: "Software Engineering",
+//     isHappy: true,
+// };
+
+// for (let key in family) {
+//     if (family.hasOwnProperty(key)) {
+//         console.log(key);
+//     }
+// }
+
+// for (let key in family) {
+//     if (family.hasOwnProperty(key)) {
+//         console.log(key + ": " + family[key]);
+//     }
+// }
+
+//exc6
+// const details = {
+//     my: 'name',
+//     is: 'Rudolf',
+//     the: 'raindeer'
+// }
+// for(let key in details){
+//     if(details.hasOwnProperty(key)){
+//         console.log(key + ' ' + details[key])
+//     }
+// }
+
+//exc7
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+const firstLetter= [];
+for (let i=0; i < names.length; i++){
+    firstLetter.push(names[i][0])
+}
+console.log('name of the secret group '+ firstLetter.join(''))
