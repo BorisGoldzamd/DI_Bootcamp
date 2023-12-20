c97fe786f4f25f772e66d305dcb39955625b9a3c
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2023-12-20 10:03:02 +0200

    Day3

---
+++ bootcamp/week4/Day3/script.js
@@ -0,0 +1,9 @@
+const myDiv = document.querySelector('div') //tambien sin selector all
+console.log(myDiv)
+const body = document.querySelector('body');
+console.log(body.firstElementChild)
+console.log(myDiv.nextSibling)
+const myUl = document.querySelector('ul')
+console.log(myUl);
+const myLi = document.querySelectorAll('li')
+console.log(myLi[1])
