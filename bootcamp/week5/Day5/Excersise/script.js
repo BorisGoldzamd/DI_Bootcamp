352c367944dd331a5ad1d9d289d54d8de7bef77c
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2023-12-30 17:27:47 +0200

    git push exc

---
+++ bootcamp/week5/Day5/Excersise/script.js
@@ -0,0 +1,24 @@
+let selectedColor = '';
+const canvasContainer = document.getElementById('canvas-container');
+
+function selectColor(color) {
+    selectedColor = color;
+}
+
+// Crea una cuadrícula de 50x50 en el lienzo
+for (let i = 0; i < 50; i++) {
+    for (let j = 0; j < 50; j++) {
+        const canvasItem = document.createElement('div');
+        canvasItem.classList.add('grid-item', 'canvas-item');
+        canvasItem.addEventListener('click', function () {
+            applyColor(canvasItem);
+        });
+        canvasContainer.appendChild(canvasItem);
+    }
+}
+
+function applyColor(canvasItem) {
+    if (selectedColor !== '') {
+        canvasItem.style.backgroundColor = document.getElementById(selectedColor).style.backgroundColor;
+    }
+}
