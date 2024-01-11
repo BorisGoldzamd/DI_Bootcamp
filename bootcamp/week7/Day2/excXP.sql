9142922205fcb0b10c4b439ec0fe4146f024c062
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2024-01-11 19:54:01 +0200

    day 2 sql

---
+++ bootcamp/week7/Day2/excXP.sql
@@ -0,0 +1,18 @@
+-- SELECT product_price, product_id, product_name from items ORDER BY product_price ASC
+
+-- SELECT product_price, product_id, product_name
+-- FROM items
+-- WHERE product_price >= 80
+-- ORDER BY product_price DESC;
+
+
+-- SELECT customer_id, customer_name, customer_lastname
+-- FROM costumers
+-- ORDER BY customer_name ASC
+-- LIMIT 3;
+
+-- SELECT customer_lastname
+-- FROM costumers
+-- ORDER BY customer_lastname DESC;
+
+
