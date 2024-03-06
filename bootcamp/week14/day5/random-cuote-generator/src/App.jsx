e55cb53c18d0dbea04e722d371aa759d25c8018a
Boris Goldzamd <125725241+BorisGoldzamd@users.noreply.github.com>
2024-03-06 10:51:10 +0200

    day 5 week 14

--- bootcamp/week14/day5/random-cuote-generator/src/App.jsx
+++ bootcamp/week14/day5/random-cuote-generator/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import RandomGenerator from './components/RandomGenerator'
 function App() {
 
   return (
     <>
+    <RandomGenerator/>
     </>
   )
 }
--- bootcamp/week14/day5/random-cuote-generator/src/components/RandomGenerator.jsx
+++ bootcamp/week14/day5/random-cuote-generator/src/components/RandomGenerator.jsx
@@ -1,16 +1,35 @@
+import React, { useState } from "react";
 import quotes from "./QuotesDatabase";
-import React, {useState} from "react";
 
-const RandomGenerator = () =>{
+const RandomGenerator = () => {
     const [quote, setQuote] = useState({
-        text: '',
-        author:''
+        text: 'Esto es una cita fija',
+        author: 'Autor fijo'
     });
-    return(
+
+    const generateRandomQuote = () => {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        console.log("Quote:", quotes[randomIndex]);
+        setQuote(quotes[randomIndex]);
+    };
+
+    console.log("All quotes:", quotes);
+
+    return (
         <div className="container">
-            <div className="quote"></div>
+            <button onClick={generateRandomQuote}>Generate Quote</button>
+            <div className="quote">
+                {quote.text && (
+                    <>
+                        <p>{quote.text}</p> 
+                        <h5 className="author">{quote.author}</h5>
+                    </>
+                )}
+            </div>
         </div>
-    )
+    );
 };
 
-export default RandomGenerator
+export default RandomGenerator;
+
+//i dont know why i been trying to debug all day long i cant find the mistake, if i do {quote.author} in consolelog i can see it but not outside. 
